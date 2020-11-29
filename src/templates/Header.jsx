607b13a8db14ed8cd97b613da1039d745e89b814
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-11-29 14:08:45 +0900

    add responsive view footer

--- src/templates/Header.jsx
+++ src/templates/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
     @media screen and (max-width: 768px) {
       background-image: none;
       padding: 0;
-      margin-bottom: 20px
+      margin-bottom: 20px;
     }
   `
 
@@ -73,6 +73,9 @@ const Header = () => {
       right: 0;
       z-index: 999;
       width: 100%;
+      @media screen and (max-width: 768px) {
+        width: 120%;
+      }
     }
   `
 
